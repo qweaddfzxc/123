864998922f3866309112a31a0919f23b4f300159
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-12 15:25:03 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,6 +1,5 @@
 import streamlit as st
 user1 = st.number_input("請輸入：剪刀：（0）、石頭（1）、布（2）")
-user1 = st.button('輸入確認')
 import random as rd
 computer = random.randint(0,2)
 if user1 == computer:
@@ -14,6 +13,7 @@ if user1 == computer:
         st.write("你的輸入為：布（2）");
         st.write("隨機生成數字為：2");
     st.write("啊哈，是平局！");
+user1 = st.button('輸入確認')
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
     rd.write("隨機生成數字為：1");
