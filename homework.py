600a65289b66c0ace5024a23fd2454f014f9d34b
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-12 15:23:38 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,7 +1,7 @@
 import streamlit as st
 user1 = st.number_input("請輸入：剪刀：（0）、石頭（1）、布（2）")
 user1 = st.button('輸入確認')
-import random 
+import random as rd
 computer = random.randint(0,2)
 if user1 == computer:
     if user1==0:
@@ -16,25 +16,25 @@ if user1 == computer:
     st.write("啊哈，是平局！");
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
-    st.write("隨機生成數字為：1");
+    rd.write("隨機生成數字為：1");
     st.write(("哈哈，你輸了"));
 elif user1 == 0 and computer == 2:
     st.write("你的輸入為：剪刀（0）");
-    st.write("隨機生成數字為：2");
+    rd.write("隨機生成數字為：2");
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 0:
     st.write("你的輸入為：石頭（1）");
-    st.write("隨機生成數字為：0");
+    rd.write("隨機生成數字為：0");
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 2:
     st.write("你的輸入為：石頭（1）");
-    st.write("隨機生成數字為：2");
+    rd.write("隨機生成數字為：2");
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 0:
     st.write("你的輸入為：布（2）");
-    st.write("隨機生成數字為：0");
+    rd.write("隨機生成數字為：0");
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 1:
     st.write("你的輸入為：布（2）");
-    st.write("隨機生成數字為：1");
+    rd.write("隨機生成數字為：1");
     st.write(("恭喜你贏啦"));
