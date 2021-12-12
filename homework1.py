2d13ffc37e6cd88c02b23614a0ca253ae647a845
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-12 15:42:43 +0800

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import random as rd
+dict(1:'剪子',2:'石頭',3:'布')
+computer = random.randint(0,2)
+user1 = st.button('輸入確認')
+if user1 == computer:
+    if user1==0:
+        st.write("你的輸入為：剪刀（0）")
+        st.write("隨機生成數字為：0")
+    elif user1==1:
+        st.write("你的輸入為：石頭（1）");
+        st.write("隨機生成數字為：1");
+    else:
+        st.write("你的輸入為：布（2）");
+        st.write("隨機生成數字為：2");
+    st.write("啊哈，是平局！");
+elif user1 == 0 and computer == 1:
+    st.write("你的輸入為：剪刀（0）");
+    rd.write("隨機生成數字為：1");
+    st.write(("哈哈，你輸了"));
+elif user1 == 0 and computer == 2:
+    st.write("你的輸入為：剪刀（0）");
+    rd.write("隨機生成數字為：2");
+    st.write(("恭喜你贏啦！"));
+elif user1 == 1 and computer == 0:
+    st.write("你的輸入為：石頭（1）");
+    rd.write("隨機生成數字為：0");
+    st.write(("恭喜你贏啦！"));
+elif user1 == 1 and computer == 2:
+    st.write("你的輸入為：石頭（1）");
+    rd.write("隨機生成數字為：2");
+    st.write(("哈哈，你輸了"));
+elif user1 == 2 and computer == 0:
+    st.write("你的輸入為：布（2）");
+    rd.write("隨機生成數字為：0");
+    st.write(("哈哈，你輸了"));
+elif user1 == 2 and computer == 1:
+    st.write("你的輸入為：布（2）");
+    rd.write("隨機生成數字為：1");
+    st.write(("恭喜你贏啦"));
