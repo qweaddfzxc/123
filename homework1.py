7ae73d9bb17d313d404339c66cf4b295d5dfd6e9
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-12 15:45:25 +0800

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random as rd
-dict(1:'剪子',2:'石頭',3:'布')
+st.dict(1:'剪子',2:'石頭',3:'布')
 computer = random.randint(0,2)
 user1 = st.button('輸入確認')
 if user1 == computer:
