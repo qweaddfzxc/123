bfc9390374ad0ad0903a98533eeb752440c97466
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-22 21:55:22 +0800

    Update 1000.py

--- 1000.py
+++ 1000.py
@@ -1,11 +1,11 @@
 import streamlit as st
 a = st.number_input('請輸入：1～99 的數字')
 confirm_input = st.button('輸入確認')
-b = confirm_input = random.randint(1,99)
+confirm_input = random.randint(1,99)
 while True:
-  if b < a:
-    b = st.write(('數字太小囉！再試一次吧：'))
-  elif b > a:
-    b = st.write(('數字太大囉！再試一次吧：'))
+  if  confirm_input< a:
+    confirm_input = st.write(('數字太小囉！再試一次吧：'))
+  elif confirm_input > a:
+    confirm_input = st.write(('數字太大囉！再試一次吧：'))
   else:
     st.write('答對囉！');
