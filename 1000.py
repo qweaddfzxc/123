d893045287200d419f092c21f2f0d51a00af890b
qweaddfzxc <95857850+qweaddfzxc@users.noreply.github.com>
2021-12-22 22:05:14 +0800

    Update 1000.py

--- 1000.py
+++ 1000.py
@@ -4,10 +4,9 @@ confirm_input = st.button('輸入確認')
 if confirm_input:
   import random
   b = random.randint(1,99)
-  while True:
-    if  b < a:
-      b = st.write(('數字太小囉！再試一次吧：'))
-    elif b > a:
-      b = st.write(('數字太大囉！再試一次吧：'))
-    else:
-      b = st.write('答對囉！');
+  if  b < a:
+    b = st.write(('數字太小囉！再試一次吧：'))
+  elif b > a:
+    b = st.write(('數字太大囉！再試一次吧：'))
+  else:
+    b = st.write('答對囉！');
